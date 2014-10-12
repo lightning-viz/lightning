8045cae8b82ad411fec3ec6f7ddab34263354ae9
Matthew Conlen <mc@mathisonian.com>
2014-10-12 17:05:05 -0400

    cleaup server.js and fix bug where server had to be restarted to bootstrap VizTypes

--- server.js
+++ server.js
@@ -3,12 +3,8 @@
  * Module dependencies
  */
 
-var fs = require('fs');
 var express = require('express');
-// var mongoose = require('mongoose');
 // var passport = require('passport');
-var config = require('./config/config');
-var _ = require('lodash');
 
 var app = express();
 var server = require('http').Server(app);
@@ -23,40 +19,9 @@ var port = process.env.PORT || 3000;
 
 var models = require('./app/models');
 models.sequelize.sync(function() {
+    require('./scripts/create_visualization_types');
 });
 
-// make sure all viz types are available.
-require('./scripts/create_visualization_types');
-
-// models.Visualization
-//     .findAll()
-//     .then(function(visualizations) {
-//         // console.log(visualizations);
-//         // console.log(visualizations.data);
-//         _.each(visualizations, function(v) {
-//             console.log(v.data);
-//             console.log(typeof v.data);
-//             v.getNamedObjectAtIndex('timeseries', 2).then(function(data) {
-//                 console.log(data);
-//             });
-//         });
-//     });
-
-
-// Connect to mongodb
-// var connect = function () {
-//   var options = { server: { socketOptions: { keepAlive: 1 } } };
-//   mongoose.connect(config.db, options);
-// };
-// connect();
-
-// mongoose.connection.on('error', console.log);
-// mongoose.connection.on('disconnected', connect);
-
-// // Bootstrap models
-// fs.readdirSync(__dirname + '/app/models').forEach(function (file) {
-//   if (~file.indexOf('.js')) require(__dirname + '/app/models/' + file);
-// });
 
 // // Bootstrap passport config
 // require('./config/passport')(passport, config);
