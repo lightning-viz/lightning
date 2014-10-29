5dca24a96601747d9009bfeed1b8b46d873ed9df
Matthew Conlen <mc@mathisonian.com>
2014-10-29 14:36:21 -0400

    dynamic css loading and manual input data

--- ui/js/utils/index.js
+++ ui/js/utils/index.js
@@ -3,7 +3,13 @@ var request = require('superagent');
 
 module.exports = {
 
+    addStylesheet: function(url) {
+        $('head').append('<link rel="stylesheet" href="' + url + '" type="text/css" />');
+    },
+
     requireOrFetchViz: function(vizName, cb) {
+
+        var self = this;
 
         try {
             console.log('trying')
@@ -16,6 +22,8 @@ module.exports = {
             console.log('fail');
 
             console.log(e);
+            self.addStylesheet('/css/dynamic/viz/?visualizations[]=' + vizName);
+
             request('/js/dynamic/viz/?visualizations[]=' + vizName, function(err, res) {
                 if(err) {
                     cb(err);
@@ -26,6 +34,8 @@ module.exports = {
                 var Viz = require('viz/' + vizName);
                 cb(null, Viz);
             });
+
+
 
 
         }
