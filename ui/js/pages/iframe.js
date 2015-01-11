ee918bb5c6ee9e6121b874ddbbfd7aaf78b34c40
Matthew Conlen <mc@mathisonian.com>
2015-01-11 18:36:37 -0500

    include pym on iframe pages

---
+++ ui/js/pages/iframe.js
@@ -0,0 +1,58 @@
+
+var sid = document.URL.substring(document.URL.lastIndexOf('/sessions/') + '/sessions/'.length);
+sid = sid.slice(0, sid.indexOf('/'));
+
+var vizs = {};
+
+
+var utils = require('../utils');
+
+function loadJS(src, callback) {
+    var s = document.createElement('script');
+    s.src = src;
+    s.async = true;
+    s.onreadystatechange = s.onload = function() {
+        var state = s.readyState;
+        if (!callback.done && (!state || /loaded|complete/.test(state))) {
+            callback.done = true;
+            callback();
+        }
+    };
+    document.getElementsByTagName('head')[0].appendChild(s);
+}
+
+loadJS(window.lightning.host + 'js/dynamic/viz/?visualizations[]=' + window.lightning.requiredVizTypes.join('&visualizations[]='), function() { 
+    
+    $('.feed-item[data-initialized=false]').each(function() {
+
+        console.log($(this));
+
+        var type = $(this).data('type');
+        var data = $(this).data('data');
+        var images = $(this).data('images');
+        var options = $(this).data('options');
+
+        var $this = $(this);
+
+
+        utils.requireOrFetchViz(type, function(err, Viz) {
+            if(err) {
+                return console.log(err);
+            }
+
+            var vid = $this.attr('id');
+            vizs[vid.slice(vid.indexOf('-') + 1)] = new Viz('#' + $this.attr('id'), data, images, options);
+            $this.data('initialized', true);
+            $this.attr('data-initialized', true);
+        });
+    });
+
+    $('.feed-container').animate({opacity: 1});
+
+
+});
+
+
+var pym = require('pym.js');
+new pym.Child();
+
