21af819cc5d1e9f15032f3c885c9b605add3581d
Matthew Conlen <mc@mathisonian.com>
2015-01-28 20:11:13 -0500

    dynamically add jquery to embed html

--- ui/js/pages/embed.js
+++ ui/js/pages/embed.js
@@ -1,4 +1,8 @@
 
+
+var jQueryURL = '//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js';
+
+
 var sid = document.URL.substring(document.URL.lastIndexOf('/sessions/') + '/sessions/'.length);
 sid = sid.slice(0, sid.indexOf('/'));
 
@@ -21,37 +25,48 @@ function loadJS(src, callback) {
     document.getElementsByTagName('head')[0].appendChild(s);
 }
 
-loadJS(window.lightning.host + 'js/dynamic/viz/?visualizations[]=' + window.lightning.requiredVizTypes.join('&visualizations[]='), function() { 
-    
-    $('.feed-item[data-initialized=false]').each(function() {
-
-        console.log($(this));
-
-        var type = $(this).data('type');
-        var data = $(this).data('data');
-        var images = $(this).data('images');
-        var options = $(this).data('options');
-
-        var $this = $(this);
 
 
-        utils.requireOrFetchViz(type, function(err, Viz) {
-            if(err) {
-                return console.log(err);
-            }
+function init() {
 
-            var vid = $this.attr('id');
-            vizs[vid.slice(vid.indexOf('-') + 1)] = new Viz('#' + $this.attr('id'), data, images, options);
-            $this.data('initialized', true);
-            $this.attr('data-initialized', true);
+    loadJS(window.lightning.host + 'js/dynamic/viz/?visualizations[]=' + window.lightning.requiredVizTypes.join('&visualizations[]='), function() { 
+        
+        $('.feed-item[data-initialized=false]').each(function() {
+
+            console.log($(this));
+
+            var type = $(this).data('type');
+            var data = $(this).data('data');
+            var images = $(this).data('images');
+            var options = $(this).data('options');
+
+            var $this = $(this);
+
+
+            utils.requireOrFetchViz(type, function(err, Viz) {
+                if(err) {
+                    return console.log(err);
+                }
+
+                var vid = $this.attr('id');
+                vizs[vid.slice(vid.indexOf('-') + 1)] = new Viz('#' + $this.attr('id'), data, images, options);
+                $this.data('initialized', true);
+                $this.attr('data-initialized', true);
+            });
         });
+
+        $('.feed-container').animate({opacity: 1});
+
+
     });
 
-    $('.feed-container').animate({opacity: 1});
 
+    // window.require = window._require;
+    // window.define = window._define;
+}
 
-});
-
-
-// window.require = window._require;
-// window.define = window._define;
+if(!window.$) {
+    loadJS(jQueryURL, init);
+} else {
+    init();
+}
