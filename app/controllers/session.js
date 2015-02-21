50e44653da77a8ad1b9b3dfc99ea52d410b2d6f4
Matthew Conlen <mc@mathisonian.com>
2015-02-21 15:59:13 -0500

    move utility function out of controller logic

--- app/controllers/session.js
+++ app/controllers/session.js
@@ -1,21 +1,13 @@
-
+'use strict';
 /*!
  * Module dependencies.
  */
 
 var _ = require('lodash');
 var multiparty = require('multiparty');
-var knox = require('knox');
-var randomstring = require('randomstring');
-var path = require('path');
-var easyimage = require('easyimage');
-var async = require('async');
 var models = require('../models');
 var Q = require('q');
-var commandExists = require('command-exists');
-var config = require('../../config/config');
-var fs = require('fs-extra');
-
+var utils = require('../utils');
 
 exports.index = function (req, res, next) {
 
@@ -204,7 +196,7 @@ exports.addData = function (req, res, next) {
         form.parse(req, function(err, fields, files) {
 
             _.each(files, function(f) {
-                thumbnailAndUpload(f, sessionId, function(err, data) {
+                utils.thumbnailAndUpload(f, {sessionId: sessionId}, function(err, data) {
 
                     if(err) {
                         console.log('error in thumbnailAndUpload');
@@ -270,8 +262,6 @@ exports.appendData = function (req, res, next) {
                             vdata[fieldName].push(req.body.data);
                             viz.data = vdata;
                         }
-
-
                     } else if(_.isUndefined(viz.data[fieldName])) {
                         viz.data[fieldName] = req.body.data;
                     } else {
@@ -321,7 +311,7 @@ exports.appendData = function (req, res, next) {
                 form.parse(req, function(err, fields, files) {
                     _.each(files, function(f) {
 
-                        thumbnailAndUpload(f, sessionId, function(err, data) {
+                        utils.thumbnailAndUpload(f, {sessionId: sessionId}, function(err, data) {
 
                             if(err) {
                                 console.log('error in thumbnailAndUpload');
@@ -405,7 +395,7 @@ exports.updateData = function (req, res, next) {
 
                 form.parse(req, function(err, fields, files) {
                     _.each(files, function(f) {
-                        thumbnailAndUpload(f, sessionId, function(err, data) {
+                        utils.thumbnailAndUpload(f, {sessionId: sessionId}, function(err, data) {
 
                             if(err) {
                                 console.log('error in thumbnailAndUpload');
@@ -446,205 +436,3 @@ exports.updateData = function (req, res, next) {
 
 };
 
-
-
-var thumbnailAndUpload = function(f, sessionId, callback) {
-
-
-
-    var staticUrl = '/';
-    if(config.url) {
-        staticUrl = 'http://' + config.url + '/';
-    }
-
-
-    // check if thumbnailing exists,
-    // and if s3 creds exist
-    var s3Exists = !!config.s3.key;
-    var s3Client = null;
-
-    if(s3Exists) {
-
-        s3Client = knox.createClient({
-            secure: false,
-            key: process.env.S3_KEY,
-            secret: process.env.S3_SECRET,
-            bucket: process.env.S3_BUCKET,
-        });
-     }
-
-    var maxWidth = 500;
-    var maxHeight = 500;
-
-    // Image file info
-    var imgPath = f[0].path;
-    var extension = path.extname(imgPath).toLowerCase();
-    var filenameWithoutExtension = path.basename(imgPath, extension);
-
-
-    var thumbnailPath;
-
-    if(process.env.NODE_ENV === 'production') {
-        thumbnailPath = path.resolve(__dirname + '/../../' + './tmp/' + filenameWithoutExtension + '_thumbnail' + extension);
-    } else {
-        thumbnailPath = path.dirname(imgPath) + filenameWithoutExtension + '_thumbnail' + extension;
-    }
-
-    // Upload paths for s3
-    var uploadName = randomstring.generate();
-    var destPath = '/sessions/' + sessionId + '/';
-    var originalS3Path = destPath + uploadName;
-    var thumbnailS3Path = destPath + uploadName + '_small';
-
-    // s3 headers
-    var headers = {
-      'x-amz-acl': 'public-read',
-      'Access-Control-Allow-Origin': '*',
-    };
-    if( extension === '.jpg' || extension === '.jpeg' ) {
-        headers['Content-Type'] = 'image/jpeg';
-    } else if (extension === '.png') {
-        headers['Content-Type'] = 'image/png';
-    }
-
-    commandExists('identify', function(err, imageMagickExists) {
-
-        if(imageMagickExists) {
-
-            easyimage
-                .info(imgPath)
-                .then(function(file) {
-                    var thumbWidth;
-                    var thumbHeight;
-
-                    console.log('outputing to: ' + thumbnailPath);
-
-                    if(file.width > file.height) {
-                        thumbWidth = Math.min(maxWidth, file.width);
-                        thumbHeight = file.height * (thumbWidth / file.width);
-                    } else {
-                        thumbHeight = Math.min(maxHeight, file.height);
-                        thumbWidth = file.width * (thumbHeight / file.height);
-                    }
-
-                    return easyimage.resize({
-                        src: imgPath,
-                        dst: thumbnailPath,
-                        width: thumbWidth,
-                        height: thumbHeight
-                    });
-                }).then(function() {
-
-                    if(s3Exists) {
-                        async.parallel([
-                            function(callback) {
-                                console.log('s3 exists');
-                                console.log('uploading image');
-                                console.log(imgPath + ':' + originalS3Path);
-                                s3Client.putFile(imgPath, originalS3Path, headers, callback);
-                            },
-                            function(callback) {
-                                console.log('uploading thumbnail');
-                                console.log(thumbnailPath + ':' + thumbnailS3Path);
-                                s3Client.putFile(thumbnailPath, thumbnailS3Path, headers, callback);
-                            }
-                        ], function(err, results) {
-                            var s3Response = results[0];
-
-                            var imgURL = 'https://s3.amazonaws.com/' + process.env.S3_BUCKET + originalS3Path;
-                            // var thumbURL = 'https://s3.amazonaws.com/' + process.env.S3_BUCKET + thumbnailS3Path;
-
-                            var imgData = imgURL;
-
-                            callback(null, {
-                                response: s3Response,
-                                imgData: imgData
-                            });
-                            
-                        });
-                    } else {
-
-                        console.log('S3 Credentials not found. Using local images');
-
-                        async.parallel([
-                            function(callback) {
-                                var outpath = path.resolve(__dirname + '../../../public/images/uploads' + originalS3Path);
-                                fs.copy(imgPath, outpath, callback);        
-                            },
-                            function(callback) {
-                                var outpath = path.resolve(__dirname + '../../../public/images/uploads' + thumbnailS3Path);
-                                fs.copy(thumbnailPath, outpath, callback);
-                            }
-                        ], function(err) {
-                            if(err) {
-                                return callback(err);
-                            }
-
-                            return callback(null, {
-                                response: 200,
-                                imgData: staticUrl + 'images/uploads' + originalS3Path
-                            });
-                        });
-                    }
-
-                }, function(err) {
-                    console.log(err);
-                    callback(err);
-                });
-        } else {
-
-            if(s3Exists) {
-                async.parallel([
-                    function(callback) {
-                        console.log(imgPath + ':' + originalS3Path);
-                        s3Client.putFile(imgPath, originalS3Path, headers, callback);
-                    },
-                    function(callback) {
-                        console.log(thumbnailPath + ':' + thumbnailS3Path);
-                        s3Client.putFile(thumbnailPath, thumbnailS3Path, headers, callback);
-                    }
-                ], function(err, results) {
-                    var s3Response = results[0];
-
-                    var imgURL = 'https://s3.amazonaws.com/' + process.env.S3_BUCKET + originalS3Path;
-                    // var thumbURL = 'https://s3.amazonaws.com/' + process.env.S3_BUCKET + thumbnailS3Path;
-
-                    var imgData = imgURL;
-
-                    callback(null, {
-                        response: s3Response,
-                        imgData: imgData
-                    });
-                    
-                });
-            } else {
-
-                console.log('S3 Credentials not found. Using local images');
-
-                async.parallel([
-                    function(callback) {
-                        var outpath = path.resolve(__dirname + '../../../public/images/uploads' + originalS3Path);
-                        console.log(outpath);
-                        fs.copy(imgPath, outpath, callback);        
-                    },
-                    function(callback) {
-                        var outpath = path.resolve(__dirname + '../../../public/images/uploads' + thumbnailS3Path);
-                        console.log(outpath);
-                        fs.copy(imgPath, outpath, callback);
-                    }
-                ], function(err) {
-                    if(err) {
-                        return callback(err);
-                    }
-
-                    return callback(null, {
-                        response: 200,
-                        imgData: staticUrl + 'images/uploads' + originalS3Path
-                    });
-                });
-            }
-        }
-    })
-
-
-};
