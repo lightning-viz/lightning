4bd17f26ca53369ffc8c63b539b44a9d79ad1e6e
Matthew Conlen <mc@mathisonian.com>
2015-03-31 21:33:43 -0400

    start rendering the visualizations

--- ui/js/views/dashboard.js
+++ ui/js/views/dashboard.js
@@ -3,6 +3,7 @@ require('../lib/modal');
 
 var request = require('superagent');
 var marked = require('marked');
+var _ = require('lodash');
 
 
 var AmpersandView = require('ampersand-view');
@@ -14,6 +15,9 @@ var DashboardView = AmpersandView.extend({
         "submit form#add-visualization": "handleAddVisulization"
     },
 
+    vizs: {},
+    dataCache: {},
+
     initialize: function() {
 
         var $el = $(this.el);
@@ -21,8 +25,8 @@ var DashboardView = AmpersandView.extend({
         var self = this;
 
         this.gridster = $el.find('.gridster ul').gridster({
-            widget_margins: [10, 10],
-            widget_base_dimensions: [width / 3, width / 6],
+            widget_margins: [40, 40],
+            widget_base_dimensions: [(width) / 3 - 80, width / 6],
             min_cols: 3,
             max_cols: 3,
             resize: {
@@ -42,9 +46,54 @@ var DashboardView = AmpersandView.extend({
                     });
                     var s = this.serialize();
                     console.log(JSON.stringify(s));
+                },
+
+                ignore_dragging: function(e) {
+                    return $(e.target).closest('.visualization').length;
                 }
-            }
+            }, 
+
         }).data('gridster');
+
+        this.$el = $el;
+        this.initializeVisualizations();
+    },
+
+    initializeVisualizations: function() {
+
+        var self = this;
+
+        this.$el.find('.dashboard-item[data-initialized=false]').each(function() {
+
+            var type = $(this).data('type');
+            
+            var data = self.getDataForDataSet($(this).data('dataset-id'));
+            console.log('got data');
+            console.log(data);
+
+
+            var images = $(this).data('images');
+            var options = $(this).data('options');
+
+            var Viz =  require('viz/' + type);
+
+            var vid = $(this).attr('id');
+            self.vizs[vid.slice(vid.indexOf('-') + 1)] = new Viz('#' + $(this).attr('id'), data, images, options);
+            $(this).data('initialized', true);
+            $(this).attr('data-initialized', true);
+        });
+
+    },
+
+    getDataForDataSet: function(dataSetId) {
+        var d;
+        if(this.dataCache[dataSetId]) {
+            d = this.dataCache[dataSetId];
+        } else {
+            d = this.$el.find('.dataset[data-model-id=' + dataSetId + ']').data('data'); 
+            this.dataCache[dataSetId] = d;
+        }
+        return _.cloneDeep(d);
     },
 
     handleAddVisulization: function(e) {
