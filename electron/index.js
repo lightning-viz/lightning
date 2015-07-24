977d6723d0419e5c58a2a3690bca1c38f0cc6733
freeman <the.freeman.lab@gmail.com>
2015-07-24 02:08:35 -0400

    Add browser open

--- electron/index.js
+++ electron/index.js
@@ -1,16 +1,20 @@
 var menubar = require('menubar');
 var ipc = require('ipc');
+var shell = require('shell')
 
 var mb = menubar({
     dir: __dirname,
-    width: 290,
+    width: 310,
     height: 82
 });
 
 mb.on('ready', function ready () {
-  // your app code here
   require('../server');
-});
+})
+
+ipc.on('browser', function browser (ev) {
+	shell.openExternal('http://localhost:3000')
+})
 
 ipc.on('terminate', function terminate (ev) {
     canQuit = true
