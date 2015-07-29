a41d1b59485f4c587537aff25232c0feaa011430
Matthew Conlen <mc@mathisonian.com>
2015-07-29 00:28:05 -0400

    trigger warner on delete viz button

--- ui/js/pages/editor.js
+++ ui/js/pages/editor.js
@@ -1,6 +1,13 @@
 require('../lib/bigSlide');
 $('.menu-link').bigSlide();
 
+
+$('[data-confirm]').click(function(e) {
+    if(!confirm($(this).data('confirm'))) {
+        e.preventDefault();
+    }
+});
+
 var React = require('react');
 
 var Editor = require('../components/editor');
