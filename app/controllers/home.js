9439303b36bfbbb91acbbf7ed317bd76e6352531
Matthew Conlen <mc@mathisonian.com>
2015-08-02 18:43:39 -0400

    remove playground

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -14,10 +14,6 @@ exports.index = function (req, res) {
     res.render('index');
 };
 
-exports.playground = function (req, res) {
-    res.render('playground');
-};
-
 exports.status = function (req, res) {
 
     Q.all([
