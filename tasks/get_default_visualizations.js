5a8f38167d319ab6fd386d5d1e19b39afb8f1825
Matthew Conlen <mc@mathisonian.com>
2015-08-02 21:25:21 -0400

    cleanup

--- tasks/get_default_visualizations.js
+++ tasks/get_default_visualizations.js
@@ -3,18 +3,26 @@ var Q = require('q');
 var _ = require('lodash');
 var config = require('../config/config');
 var defaultVisualizations = config.defaultVisualizations || [];
+require('colors');
+var npm = require('npm');
 
 module.exports = function(cb) {
+
+    console.log('\nInstalling default visualizations from npm. This may take a minute or two...'.green);                
+    var loglevel = npm.config.get('loglevel');
+    npm.config.set('loglevel', 'silent');
 
     Q.all(_.map(defaultVisualizations, function(moduleName) {
         return models.VisualizationType.createFromNPM(moduleName);
     }))
     .spread(function() {
         var vizTypes = Array.prototype.slice.call(arguments, 0);
+        npm.config.set('loglevel', loglevel);
         console.log('Created Viz Types: ' + _.pluck(vizTypes, 'name').join(', '));
         cb && cb();
     }).fail(function(err) {
         console.log(err);
+        npm.config.set('loglevel', loglevel);
         cb && cb(err);
     });
 
