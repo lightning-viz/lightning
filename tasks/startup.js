66682dd88d8a5c4be827be13d38412ff5aa19ffa
Matthew Conlen <mc@mathisonian.com>
2015-08-26 01:26:30 -0400

    revert socket for now... add sticky sessions

---
+++ tasks/startup.js
@@ -0,0 +1,41 @@
+var models = require('../app/models');
+var utils = require('../app/utils');
+var env = process.env.NODE_ENV || 'development';
+var dbConfig = require(__dirname + '/../config/database')[env];
+var _ = require('lodash');
+var tasks = require('./index');
+var port = process.env.PORT || 3000;
+
+
+
+var f = function() {
+    models.sequelize.sync({force: false})
+        .then(function() {
+
+            models.VisualizationType
+                .findAll()
+                .then(function(vizTypes) {
+                    console.log('\nInstalled visualizations:');
+                    console.log('-------------------------');
+                    _.each(vizTypes, function(vt) {
+                        console.log('* ' + vt.name);
+                    })
+                    if(vizTypes.length === 0) {
+                        tasks.getDefaultVisualizations();
+                    }
+                });
+        }).catch(function(err) {
+            console.log('Could not connect to the database. Is Postgres running?');
+            throw err;
+        });
+
+
+
+
+    console.log(utils.getASCIILogo().magenta);
+
+    console.log('Lightning started on port: ' + port);
+    console.log('Running database: ' + dbConfig.dialect);
+};
+
+module.exports = f;
